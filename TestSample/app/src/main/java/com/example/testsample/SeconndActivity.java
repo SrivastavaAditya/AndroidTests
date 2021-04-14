b057fb925e21f19cc10c6ecb3d7f40b31ab29607
Aditya Srivastava <aditya.srivastav@jublfood.com>
2021-04-14 19:54:12 +0530

    mainactivity tests

--- TestSample/app/src/main/java/com/example/testsample/SeconndActivity.java
+++ TestSample/app/src/main/java/com/example/testsample/SeconndActivity.java
@@ -1,9 +1,7 @@
 package com.example.testsample;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,30 +10,28 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class SeconndActivity extends AppCompatActivity {
 
-    @BindView(R.id.tv_main_activity_name)
+    @BindView(R.id.tv_second_activity_name)
     TextView tvActivityName;
-    @BindView(R.id.btn_next)
-    Button btnNext;
+    @BindView(R.id.btn_back)
+    Button btnBack;
 
-    private final String TAG = MainActivity.class.getSimpleName();
+    private static final String TAG = SeconndActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_seconnd);
 
         ButterKnife.bind(this);
 
         tvActivityName.setText(TAG);
-
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SeconndActivity.class));
+                onBackPressed();
             }
         });
-
     }
 }
